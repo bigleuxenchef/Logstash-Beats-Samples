d8131d8baa1799ee7c54580e266b04ce4cd71411
bigleux <bigleuxenchef@gmail.com>
2018-03-31 12:27:15 -0400

    upd

--- filebeat/src/duplicates_files.sh
+++ filebeat/src/duplicates_files.sh
@@ -1,11 +1,35 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 # rumi 2018
 
+# usage : duplicates_files.sh <file>
+# generate in the same folder as <file> 10000 files like <file><number from 1 to 10000> 
+usage()
+{
+    echo "usage: duplicates_files.sh [-f <file>] [-n <number of file to create:default 10>]| [-h]]"
+}
 
+NUMBER_OF_COPY=10 #Default value
 
-for (( i=1; i<=10000; i++))
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --file )		shift
+				INPUTFILE=$1
+                                ;;
+        -n | --number-of-copy ) shift
+				NUMBER_OF_COPY=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+for (( i=1; i<=$NUMBER_OF_COPY; i++))
 	do
-		echo "Number $i"
-		echo $1$i
-		cp $1 $1$i
+		echo -ne "Number $i  $INPUTFILE$i \r"
+		cp $INPUTFILE $INPUTFILE$i
 	done
+echo -e "\n\ndone !"
