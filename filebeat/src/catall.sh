62804e500688da5d80f22075f0cc571922f9bac2
bigleux <bigleuxenchef@gmail.com>
2018-03-31 13:33:02 -0400

    up

--- filebeat/src/catall.sh
+++ filebeat/src/catall.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash 
+# rumi 2018
+# catall.sh <arg1> <arg2>
+# <arg1> : path where all files need to be aggregated
+# <arg2> : targetfile to contain all aggregated files from path in <arg1>
 
 timestamp=$(date +%s)
 
-MYCWD=$(pwd)
+echo "source directory $1 ..."
 
-cd /usr/local/var/jenkins
 basetime=$(date +%s)
-ls -l1 | xargs -P8 -I file cat file > /tmp/test.log
+ls -l1 $1| xargs -P8 -I file cat $1/file > $2
 totaltime=$(echo "($(date +%s) - ${basetime})" | bc)
 echo "runtime: ${totaltime} seconds"
 
-cd $MYCWD
